421e55f73942047d0ec925db98ea7e99744fde0a
ภูมินทร์ สิงห์พันธ์ <77091949+Phumin-Singphan@users.noreply.github.com>
2022-10-11 00:29:14 +0700

    Add files via upload

--- webapp/showlistproduct.php
+++ webapp/showlistproduct.php
@@ -14,13 +14,18 @@ $sql = "SELECT * FROM `orderproduct`,`orderdetail`,`product` WHERE `orderproduct
 $result=mysqli_query($con,$sql);
 
 if(mysqli_num_rows($result)>0){
-
     echo "<table border=2px double><tr><th>order_id</th><th>name</th><th>fname</th><th>lname</th><th>address</th><th>mobile</th><th>price</th></tr>";
     while($row=mysqli_fetch_assoc($result)){
     echo "<tr><td>".$row["Order_ID"]."</td><td>".$row["Name"]."</td><td>".$row["FName"]."</td><td>";
     echo $row["Lname"]."</td><td>".$row["Address"]."</td><td>";
     echo $row["Mobile"]."</td><td>".$row["Price"]."</td></tr>";
     }
+    $total=0;
+    for($i=0;$i<count($_SESSION["cart"]);$i++){
+        $item=$_SESSION["cart"][$i];
+        $total+=$item['Price'];
+    }
+    echo "<h1>ราคาสินค้าทั้งหมด $total บาท</h1>";
     echo "</table>";
 }else{
     echo "0 results";
